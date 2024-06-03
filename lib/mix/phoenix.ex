f08d825d6d0c2891aa8f8045fe4c343e8b749005
c4710n <c4710n@users.noreply.github.com>
2024-06-03 15:02:55 +0800

    chore: sync

--- lib/mix/phoenix.ex
+++ lib/mix/phoenix.ex
@@ -1,64 +1,5 @@
 defmodule Mix.Phoenix do
-  # Conveniences for Phoenix tasks.
   @moduledoc false
-
-  @doc """
-  Evals EEx files from source dir.
-
-  Files are evaluated against EEx according to
-  the given binding.
-  """
-  def eval_from(apps, source_file_path, binding) do
-    sources = Enum.map(apps, &to_app_source(&1, source_file_path))
-
-    content =
-      Enum.find_value(sources, fn source ->
-        File.exists?(source) && File.read!(source)
-      end) || raise "could not find #{source_file_path} in any of the sources"
-
-    EEx.eval_string(content, binding)
-  end
-
-  @doc """
-  Copies files from source dir to target dir
-  according to the given map.
-
-  Files are evaluated against EEx according to
-  the given binding.
-  """
-  def copy_from(apps, source_dir, binding, mapping) when is_list(mapping) do
-    roots = Enum.map(apps, &to_app_source(&1, source_dir))
-
-    binding =
-      Keyword.merge(binding,
-        maybe_heex_attr_gettext: &maybe_heex_attr_gettext/2,
-        maybe_eex_gettext: &maybe_eex_gettext/2
-      )
-
-    for {format, source_file_path, target} <- mapping do
-      source =
-        Enum.find_value(roots, fn root ->
-          source = Path.join(root, source_file_path)
-          if File.exists?(source), do: source
-        end) || raise "could not find #{source_file_path} in any of the sources"
-
-      case format do
-        :text -> Mix.Generator.create_file(target, File.read!(source))
-        :eex  -> Mix.Generator.create_file(target, EEx.eval_file(source, binding))
-        :new_eex ->
-          if File.exists?(target) do
-            :ok
-          else
-            Mix.Generator.create_file(target, EEx.eval_file(source, binding))
-          end
-      end
-    end
-  end
-
-  defp to_app_source(path, source_dir) when is_binary(path),
-    do: Path.join(path, source_dir)
-  defp to_app_source(app, source_dir) when is_atom(app),
-    do: Application.app_dir(app, source_dir)
 
   @doc """
   Inflects path, scope, alias and more from the given name.
@@ -97,23 +38,25 @@ defmodule Mix.Phoenix do
 
   """
   def inflect(singular) do
-    base       = Mix.Phoenix.base()
+    base = Mix.Phoenix.base()
     web_module = base |> web_module() |> inspect()
-    scoped     = Phoenix.Naming.camelize(singular)
-    path       = Phoenix.Naming.underscore(scoped)
-    singular   = String.split(path, "/") |> List.last
-    module     = Module.concat(base, scoped) |> inspect
-    alias      = String.split(module, ".") |> List.last
-    human      = Phoenix.Naming.humanize(singular)
+    scoped = Phoenix.Naming.camelize(singular)
+    path = Phoenix.Naming.underscore(scoped)
+    singular = String.split(path, "/") |> List.last()
+    module = Module.concat(base, scoped) |> inspect
+    alias = String.split(module, ".") |> List.last()
+    human = Phoenix.Naming.humanize(singular)
 
-    [alias: alias,
-     human: human,
-     base: base,
-     web_module: web_module,
-     module: module,
-     scoped: scoped,
-     singular: singular,
-     path: path]
+    [
+      alias: alias,
+      human: human,
+      base: base,
+      web_module: web_module,
+      module: module,
+      scoped: scoped,
+      singular: singular,
+      path: path
+    ]
   end
 
   @doc """
@@ -121,8 +64,9 @@ defmodule Mix.Phoenix do
   """
   def check_module_name_availability!(name) do
     name = Module.concat(Elixir, name)
+
     if Code.ensure_loaded?(name) do
-      Mix.raise "Module name #{inspect name} is already taken, please choose another name"
+      Mix.raise("Module name #{inspect(name)} is already taken, please choose another name")
     end
   end
 
@@ -151,7 +95,7 @@ defmodule Mix.Phoenix do
   defp app_base(app) do
     case Application.get_env(app, :namespace, app) do
       ^app -> app |> to_string() |> Phoenix.Naming.camelize()
-      mod  -> mod |> inspect()
+      mod -> mod |> inspect()
     end
   end
 
@@ -177,20 +121,10 @@ defmodule Mix.Phoenix do
   end
 
   @doc """
-  The paths to look for template files for generators.
-
-  Defaults to checking the current app's `priv` directory,
-  and falls back to Phoenix's `priv` directory.
-  """
-  def generator_paths do
-    [".", :phoenix]
-  end
-
-  @doc """
   Checks if the given `app_path` is inside an umbrella.
   """
   def in_umbrella?(app_path) do
-    umbrella = Path.expand(Path.join [app_path, "..", ".."])
+    umbrella = Path.expand(Path.join([app_path, "..", ".."]))
     mix_path = Path.join(umbrella, "mix.exs")
     apps_path = Path.join(umbrella, "apps")
     File.exists?(mix_path) && File.exists?(apps_path)
@@ -223,6 +157,7 @@ defmodule Mix.Phoenix do
           {^ctx_app, path} -> Path.relative_to_cwd(path)
           _ -> mix_app_path(ctx_app, this_app)
         end
+
       Path.join(app_path, rel_path)
     end
   end
@@ -270,8 +205,9 @@ defmodule Mix.Phoenix do
     case Application.get_env(this_otp_app, :generators)[:context_app] do
       nil ->
         :error
+
       false ->
-        Mix.raise """
+        Mix.raise("""
         no context_app configured for current application #{this_otp_app}.
 
         Add the context_app generators config in config.exs, or pass the
@@ -288,9 +224,11 @@ defmodule Mix.Phoenix do
 
         Note: cli option only works when `context_app` is not set to `false`
         in the config.
-        """
+        """)
+
       {app, _path} ->
         {:ok, app}
+
       app ->
         {:ok, app}
     end
@@ -300,11 +238,12 @@ defmodule Mix.Phoenix do
     case Mix.Project.deps_paths() do
       %{^app => path} ->
         Path.relative_to_cwd(path)
-      deps ->
-        Mix.raise """
-        no directory for context_app #{inspect app} found in #{this_otp_app}'s deps.
 
-        Ensure you have listed #{inspect app} as an in_umbrella dependency in mix.exs:
+      deps ->
+        Mix.raise("""
+        no directory for context_app #{inspect(app)} found in #{this_otp_app}'s deps.
+
+        Ensure you have listed #{inspect(app)} as an in_umbrella dependency in mix.exs:
 
             def deps do
               [
@@ -315,35 +254,9 @@ defmodule Mix.Phoenix do
 
         Existing deps:
 
-            #{inspect Map.keys(deps)}
+            #{inspect(Map.keys(deps))}
 
-        """
-    end
-  end
-
-  @doc """
-  Prompts to continue if any files exist.
-  """
-  def prompt_for_conflicts(generator_files) do
-    file_paths =
-      Enum.flat_map(generator_files, fn
-        {:new_eex, _, _path} -> []
-        {_kind, _, path} -> [path]
-      end)
-
-    case Enum.filter(file_paths, &File.exists?(&1)) do
-      [] -> :ok
-      conflicts ->
-        Mix.shell().info"""
-        The following files conflict with new files to be generated:
-
-        #{Enum.map_join(conflicts, "\n", &"  * #{&1}")}
-
-        See the --web option to namespace similarly named resources
-        """
-        unless Mix.shell().yes?("Proceed with interactive overwrite?") do
-          System.halt()
-        end
+        """)
     end
   end
 
@@ -355,51 +268,6 @@ defmodule Mix.Phoenix do
       Module.concat([base])
     else
       Module.concat(["#{base}Web"])
-    end
-  end
-
-  def to_text(data) do
-    inspect data, limit: :infinity, printable_limit: :infinity
-  end
-
-  def prepend_newline(string) do
-    "\n" <> string
-  end
-
-  # In the context of a HEEx attribute value, transforms a given message into a
-  # dynamic `gettext` call or a fixed-value string attribute, depending on the
-  # `gettext?` parameter.
-  #
-  # ## Examples
-  #
-  #     iex> ~s|<tag attr=#{maybe_heex_attr_gettext("Hello", true)} />|
-  #     ~S|<tag attr={gettext("Hello")} />|
-  #
-  #     iex> ~s|<tag attr=#{maybe_heex_attr_gettext("Hello", false)} />|
-  #     ~S|<tag attr="Hello" />|
-  defp maybe_heex_attr_gettext(message, gettext?) do
-    if gettext? do
-      ~s|{gettext(#{inspect(message)})}|
-    else
-      inspect(message)
-    end
-  end
-
-  # In the context of an EEx template, transforms a given message into a dynamic
-  # `gettext` call or the message as is, depending on the `gettext?` parameter.
-  #
-  # ## Examples
-  #
-  #     iex> ~s|<tag>#{maybe_eex_gettext("Hello", true)}</tag>|
-  #     ~S|<tag><%= gettext("Hello") %></tag>|
-  #
-  #     iex> ~s|<tag>#{maybe_eex_gettext("Hello", false)}</tag>|
-  #     ~S|<tag>Hello</tag>|
-  defp maybe_eex_gettext(message, gettext?) do
-    if gettext? do
-      ~s|<%= gettext(#{inspect(message)}) %>|
-    else
-      message
     end
   end
 end
